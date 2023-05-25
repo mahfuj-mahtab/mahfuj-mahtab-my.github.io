435ce3f2587da63365b2392a9f61c97696ecd139
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-05-25 10:57:23 -0400

    done

--- Asset/js/main.js
+++ Asset/js/main.js
@@ -1,8 +1,19 @@
 var cursor = document.getElementsByClassName("cursor");
 var cursorDot = document.getElementsByClassName("cursorDot");
 document.addEventListener('mousemove',(e)=>{
-    console.log(cursor[0].style)
+    // console.log(cursor[0].style)
     cursor[0].style.cssText = cursorDot[0].style.cssText = "left: "+ e.clientX  + "px;top:" + e.clientY+ "px;";
-    console.log("moving")
+    // console.log("moving")
 
 })
+
+
+
+typingspan = document.getElementsByClassName("typing-text")
+textArray = ['Website developer','Website Designer']
+charIndex = 0;
+type = ()=>{
+    // typingspan. "HH"
+    console.log(typingspan)
+}
+type()
